688f5f81884a38de12a8571da075238a6e45820e
Jimmy Ng <jimmy_ng@outlook.de>
2023-08-28 17:31:24 +0200

    adds styling to pages and posts

---
+++ page.php
@@ -0,0 +1,11 @@
+<?php
+get_header();
+?>
+
+<p>This page was created with page.php</p>
+<h1><?php the_title(); ?></h1>
+<p><?php the_content(); ?></p>
+
+<?php
+get_footer();
+?>
---
+++ single.php
@@ -0,0 +1,10 @@
+<?php
+get_header();
+?>
+
+<h1><?php the_title(); ?></h1>
+<p><?php the_content(); ?></p>
+
+<?php
+get_footer();
+?>
